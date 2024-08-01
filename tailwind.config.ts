1ee28ba3fcd34d6ff88f6bb6ba34f23bb13360a6
nasrullohfajar <nasrullohfajar10@gmail.com>
2024-08-01 17:22:43 +0700

    black color config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        black: {
+          DEFAULT: "#0A0A0A",
+          900: "#212631",
+          800: "#373F4E",
+          700: "#4E576A",
+          600: "#667085",
+        },
       },
     },
   },

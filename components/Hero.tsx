457a7757c7f496492e5656afa1e324d12e674150
nasrullohfajar <nasrullohfajar10@gmail.com>
2024-08-07 19:46:40 +0700

    add: text hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
-  return <div className="pb-20 pt-36 h-screen"></div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="max-w-[85vw] md:max-w-[70vw] lg:max-w-[64vw] flex flex-col items-center justify-center">
+        <h2 className="uppercase tracking-widest text-sm text-center text-blue-100 max-w-80">
+          Hello World!
+        </h2>
+        <TextGenerateEffect words="I'm Nasrulloh Fajar Muharam a Front end Developer" />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;

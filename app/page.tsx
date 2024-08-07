56e2b3ade7c4e3be0aa20c7aeb93be05bb9ff654
nasrullohfajar <nasrullohfajar10@gmail.com>
2024-08-07 21:25:33 +0700

    add: navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto px-5 sm:px-10">
       <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={[
+            { name: "About", link: "#about" },
+            { name: "Project", link: "#project" },
+            { name: "Contact", link: "#contact" },
+          ]}
+        />
         <Hero />
+        <p className="h-screen">das</p>
       </div>
     </main>
   );
